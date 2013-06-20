c95343b6701c50c00a81086028555aaefa48a958
Josh Manley <jmanley4@gmail.com>
2013-06-20 11:21:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ManleyTutors::Application.config.secret_token = 'f62505dd89728e4ff154234bdb4f0d2da387c8c7783ddbc9412fb0d73ccec752d2c02e2db4d63360d9587c5647549fadf7148851b05d2945d3b58e3f23037372'
